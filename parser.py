5027b476148aac55273bf7d3f9c53ac9ab2e6166
ajwaitz <waitz@stanford.edu>
2024-08-02 23:24:57 -0700

    chore: Add function to get JavaScript file paths in directory

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import subprocess
 import json
 import networkx as nx
 import sys
+import os
 import matplotlib.pyplot as plt
 
 def get_func_dependencies_json(paths):
@@ -30,12 +31,20 @@ def create_dependency_graph(paths):
 
     return graph
 
+def get_js_file_paths(directory):
+    js_file_paths = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".js"):
+                js_file_paths.append(os.path.join(root, file))
+    return js_file_paths
+
 def plot_graph(graph):
     nx.draw(graph, with_labels=True, node_color='lightblue', node_size=500, font_weight='bold', edge_color='gray')
     plt.show()
 
 if __name__ == "__main__":
-    paths = sys.argv[1:]
-    print(paths)
+    dir = sys.argv[1]
+    paths = get_js_file_paths(dir)
     G = create_dependency_graph(paths)
     plot_graph(G)
