4da6f3a306ce5052c1100670566d7782c2473798
Niall Kehoe <nkehoegames@gmail.com>
2024-08-03 20:36:15 -0700

    timeStamp

--- web/src/components/history/HistoryItem.ts
+++ web/src/components/history/HistoryItem.ts
@@ -12,16 +12,32 @@ export class HistoryItem {
     key: string,
     type: ItemType,
     pathFileName: string,
+    timeStamp: string,
     description: string = '',
-    timeStamp: string = '',
     inProgress: boolean = false,
   ) {
     this.key = key
     this.type = type
     this.pathFileName = pathFileName
+    this.timeStamp = this.processTimeStamp(timeStamp);
     this.description = description
-    this.timeStamp = timeStamp
     this.inProgress = inProgress
+    
+  }
+
+  private processTimeStamp(timeStamp: string): string {
+    const date = new Date(timeStamp);
+
+    const pad = (num: number) => num.toString().padStart(2, '0');
+
+    const month = pad(date.getMonth() + 1);
+    const day = pad(date.getDate());
+    const year = date.getFullYear();
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+    const seconds = pad(date.getSeconds());
+
+    return `${month}/${day}/${year} ${hours}:${minutes}:${seconds} PDT`;
   }
 }
 
